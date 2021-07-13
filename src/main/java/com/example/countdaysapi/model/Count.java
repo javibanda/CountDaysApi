db2ea59fc18d8b4641a3956988b689f3a51402f9
narutojavibanda <narutojavibanda@gmail.com>
2021-07-13 19:51:09 +0200

    Create entity in Count.java

--- src/main/java/com/example/countdaysapi/model/Count.java
+++ src/main/java/com/example/countdaysapi/model/Count.java
@@ -1,4 +1,26 @@
 package com.example.countdaysapi.model;
 
+
+import com.example.countdaysapi.model.day.Day;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data @NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "count")
 public class Count {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private int count;
+
+    @ManyToOne(targetEntity = Day.class)
+    @JoinColumn(name="day_id", referencedColumnName = "id")
+    private Day day;
+
 }
