3278c1987fc19ccf12c4c586fe166b903f80b5e2
javi <narutojavibanda@gmail.com>
2021-07-13 23:29:47 +0200

    Create attributes and fun in class Church

--- src/main/java/com/example/countdaysapi/model/Church.java
+++ src/main/java/com/example/countdaysapi/model/Church.java
@@ -1,4 +1,63 @@
 package com.example.countdaysapi.model;
 
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Locale;
+import java.util.Objects;
+
+@NoArgsConstructor
+@Entity
+@Getter
+@Data
+@Table(name = "church")
 public class Church {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String churchName;
+    private String city;
+
+    private String priestName;
+    private String priestLastName;
+    private String priestSecondLastName;
+
+    private String priestUser;
+    private String pass;
+
+    private LocalDate ordinationDay;
+
+    public Church(
+            String churchName,
+            String city,
+            String priestName,
+            String priestLastName,
+            String priestSecondLastName,
+            String pass,
+            LocalDate ordinationDay) {
+
+        this.churchName = churchName;
+        this.city = city;
+        this.priestName = priestName;
+        this.priestLastName = priestLastName;
+        this.priestSecondLastName = priestSecondLastName;
+        this.priestUser = setPriestUser();
+        this.pass = pass;
+        this.ordinationDay = ordinationDay;
+    }
+
+    private String setPriestUser(){
+        return getFirstLetters(priestName)
+                + getFirstLetters(priestLastName)
+                + getFirstLetters(priestSecondLastName);
+    }
+
+    private String getFirstLetters(String string){
+        return Objects.requireNonNullElse(string.substring(0,3), "").toLowerCase(Locale.ROOT);
+    }
 }
