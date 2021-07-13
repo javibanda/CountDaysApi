b470a238a804d9619e3d1c463cf8027eaeeb85fc
narutojavibanda <narutojavibanda@gmail.com>
2021-07-13 19:02:28 +0200

    Create hibernate annotations

--- src/main/java/com/example/countdaysapi/model/day/Day.java
+++ src/main/java/com/example/countdaysapi/model/day/Day.java
@@ -3,31 +3,32 @@ package com.example.countdaysapi.model.day;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+
+@Entity
 @Getter
 @Data
+@Table(name = "day")
+@NoArgsConstructor
 public class Day {
 
+    @Id
+    private Long id;
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private LocalDate date;
+    private int count;
 
     public Day(int count){
         this.count = count;
         this.date = LocalDate.now();
         this.id = new SetIdDay(date).getIdDay();
     }
-
-//    public Checking(int count, LocalDate date){
-//        this.count = count;
-//        this.date = date;
-//        this.id = new SetIdDate(date).getIdDay();
-//    }
-
-
-    private Long id;
-    @JsonFormat(pattern="yyyy-MM-dd")
-    private LocalDate date;
-    private int count;
 
     public void addCount(int add){
         count = count + add;
