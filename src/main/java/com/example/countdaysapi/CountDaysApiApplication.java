4318cf019d4fabaf14bba5039dfd9be1b33e6f1a
narutojavibanda <narutojavibanda@gmail.com>
2021-07-13 18:44:09 +0200

    Begin

---
+++ src/main/java/com/example/countdaysapi/CountDaysApiApplication.java
@@ -0,0 +1,17 @@
+package com.example.countdaysapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountDaysApiApplication {
+
+    public static void main(String[] args) {
+//        LocalDate date = LocalDate.of(2021, Month.JULY, 14);
+//        Checking x = new Checking(3, date);
+//        x.addCount(3);
+//        System.out.println(x);
+        SpringApplication.run(CountDaysApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/countdaysapi/CountDaysApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.countdaysapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountDaysApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
